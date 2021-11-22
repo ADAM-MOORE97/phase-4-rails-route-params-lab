20cdf50e5331984b6b915612bbdea42a691b352c
Adam <adammoore118@outlook.com>
2021-11-22 12:56:57 -0500

    commit tree ac15bdeff76357db2c289b2a2415765b9ce503e4

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+  get '/students?first_name=:firstname&last_name=:lastname', to: 'students#index'
+  get '/students/:id', to: 'students#show'
 end
